ceb047450abbfaa680a880ff166596cfd8eb4fde
TheAnoti <p.matthieu@live.fr>
2024-10-24 20:49:09 +0000

    fnl 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+int i;
+
+for (i = 0; alphabet[i] != '\0'; i++)
+{
+_putchar(alphabet[i]);
+}
+}
