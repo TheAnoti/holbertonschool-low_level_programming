aeeeb302c29b9defd3aac6dd3ec1c109ef39a5d1
TheAnoti <p.matthieu@live.fr>
2024-10-26 18:22:07 +0000

    fnl 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @n: The integer to get the last digit from
+ *
+ * Return: The value of the last digit
+ */
+int print_last_digit(int n)
+{
+int last_digit = n % 10;
+
+if (last_digit < 0)
+last_digit = -last_digit;
+
+_putchar(last_digit + '0');  /* Convert digit to ASCII and print */
+return (last_digit);
+}
