3b7b9781ba17accea0d178870d474a2d199f5524
TheAnoti <p.matthieu@live.fr>
2024-10-29 17:03:38 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Multiplie deux entiers.
+ * @a: Premier entier.
+ * @b: Deuxième entier.
+ *
+ * Return: Le produit de a et b.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
