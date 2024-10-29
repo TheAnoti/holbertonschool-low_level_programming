d062c28ecde46f99f8454d3abe3026e021e6e22f
TheAnoti <p.matthieu@live.fr>
2024-10-29 19:59:49 +0000

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,42 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Programme Fizz-Buzz qui affiche les nombres de 1 à 100
- * en remplaçant certains nombres par "Fizz", "Buzz" ou "FizzBuzz".
+ * fizz_buzz - Determines what to print for a given number.
+ * @n: The number to evaluate.
  *
- * Return: Toujours 0 (succès)
+ * Return: 1 if "FizzBuzz" or "Fizz" or "Buzz" was printed, otherwise 0.
+ */
+int fizz_buzz(int n)
+{
+if (n % 3 == 0 && n % 5 == 0)
+{
+printf("FizzBuzz");
+return 1;
+}
+else if (n % 3 == 0)
+{
+printf("Fizz");
+return 1;
+}
+else if (n % 5 == 0)
+{
+printf("Buzz");
+return 1;
+}
+return 0;
+}
+
+/**
+ * main - Fizz-Buzz program that prints numbers from 1 to 100.
+ *
+ * Return: Always 0 (success).
  */
 int main(void)
 {
-for (int i = 1; i <= 100; i++)
+int i;
+for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz");
-else if (i % 3 == 0)
-printf("Fizz");
-else if (i % 5 == 0)
-printf("Buzz");
-else
+if (!fizz_buzz(i))
 printf("%d", i);
 
 if (i < 100)
