a378bee433afa4fff5e71833b6a812a65fab3c2f
TheAnoti <p.matthieu@live.fr>
2024-11-02 14:27:13 +0000

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * leet - Encode une chaîne en 1337.
- * @str: Pointeur vers la chaîne de caractères.
+ * leet - Encode une chaîne de caractères en 1337.
+ * @str: La chaîne de caractères à encoder.
  *
- * Return: Pointeur vers la chaîne modifiée.
+ * Return: Pointeur vers la chaîne encodée.
  */
 char *leet(char *str)
 {
-char letters[] = "aAeEoOtTlL";
-char leet_chars[] = "430071";
-int i, j;
+    char letters[] = "aAeEoOtTlL";
+    char leet_chars[] = "43071";
+    int i, j;
 
 for (i = 0; str[i] != '\0'; i++)
 {
