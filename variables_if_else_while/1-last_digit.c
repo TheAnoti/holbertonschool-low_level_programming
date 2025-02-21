ca0b95f521b456d3df0a663dd6bb975299e75c84
TheAnoti <p.matthieu@live.fr>
2025-02-21 22:13:43 +0000

    assign a random number to the variable

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - print a rand numb and tell if it greater than five and less than six
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n, y;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	y = n % 10;
+	printf("Last digit of %d is %d ", n, y);
+	if (y > 5)
+	{
+		printf(" and is greater than 5\n");
+	} else if (y == 0)
+	{
+		printf(" and is 0\n");
+	} else
+	{
+		printf(" and is less than 6 and not 0\n");
+	}
+	return (0);
+}
