a29f364cb7326217b22ebdf7688fb28e75f2965c
TheAnoti <p.matthieu@live.fr>
2025-02-24 10:08:31 +0000

    trying another ways for the text

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,16 +17,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	y = n % 10;
-	printf("Last digit of %d is %d ", n, y);
+
+	/*printf(\"Last digit of %d is %d\", n, y);*/
 	if (y > 5)
 	{
-		printf(" and is greater than 5\n");
+		printf("Last digit of %d is %d and is greater than 5\n", n, y);
 	} else if (y == 0)
 	{
-		printf(" and is 0\n");
+		printf("Last digit of %d is %d and is 0\n", n, y);
 	} else
 	{
-		printf(" and is less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
 	}
 	return (0);
 }
