6b7cc7c7762b4a63eb4e4b83077240997295c02e
TheAnoti <p.matthieu@live.fr>
2025-02-25 14:16:05 +0000

    print num base 10 starting from 0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit on base ten
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
